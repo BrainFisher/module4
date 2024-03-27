ede6a62d09e63ce1a560e4a64e83307d21588db3
BrainFisher <brainfisher13@gmail.com>
2024-03-27 21:06:05 +0200

    task4

--- task2.py
+++ task2.py
@@ -1,6 +1,6 @@
 def get_cats_info(path):
     cats_info = []
-
+    
     try:
         with open(path, 'r', encoding='utf-8') as file:
             for line in file:
@@ -10,7 +10,7 @@ def get_cats_info(path):
                     cats_info.append(cat_info)
                 except ValueError:
                     print(f"Помилка: некоректні дані у файлі: {line.strip()}")
-
+    
         return cats_info
     except FileNotFoundError:
         print(f"Помилка: файл не знайдено за шляхом {path}")
@@ -19,8 +19,6 @@ def get_cats_info(path):
         print(f"Помилка: {e}")
         return []
 
-
 # Приклад використання з поданим шляхом
-cats_info = get_cats_info(
-    r"C:\Users\User\Desktop\My_repo\module4\cats_file.txt")
+cats_info = get_cats_info(r"C:\Users\User\Desktop\My_repo\module4\cats_file.txt")
 print(cats_info)
